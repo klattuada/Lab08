73c08cd431fc3cc6b15a82d449e4a897b9d9a6a7
klattuada <90414276+klattuada@users.noreply.github.com>
2023-03-27 23:16:47 -0400

    adding in hello world file

---
+++ helloworld.py
@@ -0,0 +1,7 @@
+print ("Hello World")
+print("Hello World, my name is Kevin and I love Python!")
+
+userName = input ("Hello! Please enter your name!")
+
+print("Hello ", userName, "nice to meet you")
+
